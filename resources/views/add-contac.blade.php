f1bb69406b8f0e73f79b2cca4f38375f13390fdc
Jonathan0890 <jonathan.coox15@gmail.com>
2024-03-20 15:41:28 -0500

    Gracias Aleman con mi proyecto

---
+++ resources/views/add-contac.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <form>
+        <input type="hidden" name="" id="name" value="">
+        <input type="hidden" name="" id="email" value="">
+    </form>
+    <h1>HOLA XD</h1>
+    <h6>Formulario</h6>
+</div>
